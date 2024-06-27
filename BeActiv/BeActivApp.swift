b99211139b92dcbdfb7186548dc6e0110382722a
arsverma5 <147878019+arsverma5@users.noreply.github.com>
2024-06-27 11:32:01 -0400

    Initial Commit

---
+++ BeActiv/BeActivApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeActivApp.swift
+//  BeActiv
+//
+//  Created by Arshia Verma on 6/27/24.
+//
+
+import SwiftUI
+
+@main
+struct BeActivApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
